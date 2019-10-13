e77e1f9fd60d9dc1df6536dc72b2b395d35bdfa0
Nick <niiickolay@gmail.com>
2019-10-13 20:58:13 +0300

    Add .env file for with environment variables

--- app/services/imagesService.js
+++ app/services/imagesService.js
@@ -1,14 +1,13 @@
 import axios from 'axios';
-
-const token =
-  'cf49c08b444ff4cb9e4d126b7e9f7513ba1ee58de7906e4360afc1a33d1bf4c0';
+import {CLIENT_ID} from 'react-native-dotenv';
 
 export const getImages = async () => {
-  const endpoint = `https://api.unsplash.com/photos/?client_id=${token}`;
+  const endpoint = `https://api.unsplash.com/photos/?client_id=${CLIENT_ID}`;
   try {
     const response = await axios.get(endpoint);
+    console.log(response);
     return response.data;
   } catch (error) {
-    console.error(error);
+    throw error;
   }
 };
